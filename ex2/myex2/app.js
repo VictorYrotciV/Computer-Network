d41b1c4052c60c8122cea8c2b4da35d559048f6c
huakaifuguiyhz <307816938@qq.com>
2022-10-28 17:14:27 +0800

    use express static, update reports

--- ex2/myex2/app.js
+++ ex2/myex2/app.js
@@ -1,21 +1,20 @@
 //引入exprexx模块
 var express =require("express");
+var path = require('path');
 var app =express();
-app.use(express.static('public'))
-
-//参数‘/’可当作设置url的根显示页面，这里即”http://localhost:3000/“访问的页面设置为index.html
+app.use(express.static(path.join(__dirname, 'public')));
+//参数‘/’可当作设置url的根显示页面
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/"+"my1.html")        //设置/ 下访问文件位置
 });
 
-//参数‘/tow’可当作设置url的/tow下显示页面，这里即”http://localhost:3000/tow“访问的页面设置为index2.html
 app.get("/yhz",(req,res)=>{
     res.sendFile(__dirname+"/"+"my2.html")        //设置/tow下访问文件位置
 })
 
-//设置端口3000访问地址，即http://localhost:3000
+
 var server =app.listen(5678,()=>{
     var port =server.address().port
-    console.log("访问地址http://localhost:%s",port)
+    console.log("server start",port)
 })
 module.exports=app;
